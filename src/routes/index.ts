d3219b687e933e9c095a315e1a46c3e2e6c2213f
sreyom31 <sreyom31@gmail.com>
2023-02-13 12:01:51 +0530

    routes and custom types
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

---
+++ src/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+const router = Router();
+
+const defaultRoutes = [
+];
+
+defaultRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+export default router;
