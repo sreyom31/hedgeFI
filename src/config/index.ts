8d30c0fd94496dc189e03116d71863fa35b9d461
sreyom31 <sreyom31@gmail.com>
2023-02-13 11:58:00 +0530

    configs
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

---
+++ src/config/index.ts
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import z from 'zod';
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['production', 'development', 'test']),
+  PORT: z.string().default('5000'),
+  DB_URL: z
+    .string()
+    .startsWith('mongodb://', { message: 'Must provide a MongoDB URL' }),
+});
+
+const envVars = envSchema.safeParse(process.env);
+if (!envVars.success) {
+  throw new Error(
+    envVars.error.issues
+      .map((error) => `${error.path} ${error.message}`)
+      .join('\n')
+  );
+}
+
+export default {
+  env: envVars.data.NODE_ENV,
+  port: envVars.data.PORT,
+  mongoose: {
+    url:
+      envVars.data.DB_URL + (envVars.data.NODE_ENV === 'test' ? '-test' : ''),
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ignoreUndefined: true,
+    },
+  },
+};
